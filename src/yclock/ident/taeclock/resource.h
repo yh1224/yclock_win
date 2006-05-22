719e9dd11b00e47f08cfd40ef550f131a205940f
hirose <hirose@47bb2106-ec12-0410-b687-d0fc2a23091c>
2006-05-22 13:48:26 +0000

    yclockの変更をtaeclockにマージ
    git-svn-id: file:///home/hirose/software.svn/yclock/trunk@42 47bb2106-ec12-0410-b687-d0fc2a23091c

--- src/yclock/ident/taeclock/resource.h
+++ src/yclock/ident/taeclock/resource.h
@@ -4,13 +4,15 @@
 //
 #define IDS_CONF_OUTOFRANGE             6
 #define IDS_CONF_INVALIDDIGIT           7
-#define IDI_ICON1                       101
 #define IDI_NORMAL                      101
 #define IDR_MENU1                       102
 #define IDD_CONFIG                      103
 #define IDD_ABOUT                       104
-#define IDD_NTP                         109
+#define IDD_SNTP                        109
+#define IDB_ILLUST                      120
+#define IDB_MONTH                       140
 #define IDD_SYNC                        141
+#define IDI_BW                          143
 #define IDC_CONFIG_SYNC                 1002
 #define IDC_CONFIG_VOICE0               1007
 #define IDC_CONFIG_VOICE60              1008
@@ -26,6 +28,13 @@
 #define IDC_ABOUT_TITLE                 1034
 #define IDC_CONFIG_STARTUP              1035
 #define ID_SYNC_PROCMSG                 1038
+#define IDC_NTP_SHIFT                   1053
+#define IDC_NTP_SHIFT_SEC               1054
+#define IDC_NTP_MAXDELAY                1055
+#define ID_SYNC_DETAILMSG               1057
+#define IDC_CONFIG_VOL_LOW              1058
+#define IDC_CONFIG_VOL_MID              1059
+#define IDC_CONFIG_VOL_HIGH             1060
 #define IDS_NTP_INIT_NG                 12301
 #define IDS_NTP_SEND_NG                 12302
 #define IDS_NTP_WAITING                 12303
@@ -39,17 +48,22 @@
 #define IDS_NTP_TIMEOUT                 12311
 #define IDS_NTP_RESOLVING               12312
 #define IDS_NTP_NOTREADY                12313
+#define IDS_NTP_RECVPKTINFO             12314
+#define IDS_NTP_RECVTIME                12315
+#define IDS_NTP_DELAY                   12316
 #define ID_MENU_EXIT                    40001
 #define ID_MENU_CONFIG                  40002
 #define ID_MENU_ABOUT                   40004
+#define ID_MENU_VIEW                    40016
+#define ID_MENU_ONTOP                   40020
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        143
+#define _APS_NEXT_RESOURCE_VALUE        145
 #define _APS_NEXT_COMMAND_VALUE         40021
-#define _APS_NEXT_CONTROL_VALUE         1043
+#define _APS_NEXT_CONTROL_VALUE         1061
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
